2796ad835a50e92703df0277006bbe1be17ec4f2
Illia Ovcharuk <ilaovcaruk0@gmail.com>
2025-07-30 18:18:16 +0100

    feat: connect to PostgreSQL database via pgAdmin

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import { sequelize } from "./db.js";
+import * as models from "./models/models.js";
+
 import dotenv from "dotenv";
 import cors from "cors";
 import ErrorHandler from "./middleware/errorHandler.js";
@@ -13,6 +16,14 @@ app.use(cors());
 
 app.use(ErrorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ models/models.js
@@ -0,0 +1,18 @@
+import { sequelize } from "../db.js";
+import { DataTypes } from "sequelize";
+
+const User = sequelize.define("user", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  email: { type: DataTypes.STRING, unique: true, allowNull: false },
+  password: { type: DataTypes.STRING, allowNull: false },
+  role: { type: DataTypes.STRING, defaultValue: "USER" },
+});
+
+const Post = sequelize.define("post", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  title: { type: DataTypes.STRING, allowNull: false },
+  content: { type: DataTypes.TEXT, allowNull: false },
+  userId: { type: DataTypes.INTEGER, allowNull: false },
+});
+
+export { User, Post };
