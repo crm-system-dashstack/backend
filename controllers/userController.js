286dee1012312b2026ab7aa39927ba45c85a97bb
Illia Ovcharuk <ilaovcaruk0@gmail.com>
2025-07-30 22:14:50 +0100

    add: bcrypt dependency for password hashing

---
+++ controllers/userController.js
@@ -0,0 +1,17 @@
+import ApiError from "../utils/apiError";
+import bcrypt from "bcrypt";
+import { User } from "../models/models.js";
+
+const generateJwtToken = () => {};
+
+export class userController {
+  async registration(req, res, next) {}
+  async login(req, res, next) {}
+  async check(req, res, next) {}
+  async deleteUser(req, res, next) {}
+  async updateProfile(req, res, next) {}
+  async getCurrentUser(req, res, next) {}
+  async forgotPassword(req, res, next) {}
+  async resetPassword(req, res, next) {}
+  async logout(req, res, next) {}
+}
